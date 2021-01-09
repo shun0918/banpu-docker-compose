03f54bf9a705955f375123b58fa73007b655bb79
shun0918 <kkabosu386@gmail.com>
2021-01-09 12:58:56 +0900

    コメントアウト削除

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!bin/sh
 
+# 全てのイメージをビルドしたい場合
+# sh build.sh all
+
+# webは時間がかかるので、端折りたい時は、
+# sh build.sh
+
 docker build -t banpu/banpu-nginx:latest ./nginx
 docker build -t banpu/banpu-varnish:latest ./varnish
 
-option=${1:-}
-echo $option
+option=${1:-} #第一引数を取得できなければ、から文字を渡す
 
 if [ "$option" == "all" ]
 then
-    echo "build banpu-web..."
+    echo "pulling banpu-web..."
+    git pull $GITHUB_WEB_REPOSITORY_PATH
+    echo "building banpu-web..."
     docker build -t banpu/banpu-web:latest ./web
 fi
